49cc2610b9ab1ff77db3c90c8046d77f54579c21
kinako890419 <kinako890419@gmail.com>
2025-09-17 02:14:45 +0800

    fix: proxy server (for claude desktop demo openapi server)

--- proxy_server.py
+++ proxy_server.py
@@ -1,5 +1,6 @@
 import os
 from fastmcp import FastMCP
+from fastmcp.client import StreamableHttpTransport
 from fastmcp.server.proxy import ProxyClient
 
 
@@ -7,9 +8,14 @@ REMOTE_MCP_URL = "http://127.0.0.1:8000/mcp"
 TOKEN = os.getenv("API_TOKEN")
 headers = {"Authorization": f"Bearer {TOKEN}"} if TOKEN else {}
 
+transport = StreamableHttpTransport(
+    url=REMOTE_MCP_URL,
+    headers={"Authorization": f"Bearer {TOKEN}"} if TOKEN else {}
+)
+
 
 proxy = FastMCP.as_proxy(
-    ProxyClient(REMOTE_MCP_URL),
+    ProxyClient(transport=transport),
     name="OpenAPI MCP Proxy"
 )
 
